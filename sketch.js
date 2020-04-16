1f5ebf4d76995359fc457fc43ecc78df1ebf0686
inovrconsultoria <63504941+inovrconsultoria@users.noreply.github.com>
2020-04-16 11:25:53 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,13 +1,21 @@
 let permissionGranted = false
 let nonios13device = false
+let cx, cy
 
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  colorMode(HSB, 360, 100, 100, 100)
+  
+  cx = width / 2
+  cy = height / 2
+  textSize(36)
 
   if (typeof(DeviceOrientationEvent) !== 'undefined' && typeof(DeviceOrientationEvent.requestPermission) === 'function') {
     DeviceOrientationEvent.requestPermission()
       .catch(() => {
         // show permission dialog only the first time
         // it needs to be a user gesture (requirement) in this case, click
-        let askButton = createButton("Permissão de acesso ao giroscópio")
+        let askButton = createButton("Allow acess to sensors")
         askButton.style("font-size", "24px")
         askButton.position(0, 0)
         askButton.mousePressed(onAskButtonClicked)
@@ -19,6 +27,7 @@ let nonios13device = false
       })
   } else {
     // it's up to you how to handle non ios 13 devices
+    text("non ios 13 devices", 50, 50)
     nonios13device = true
   }
 }
@@ -33,4 +42,23 @@ function onAskButtonClicked() {
     }
     this.remove()
   }).catch(console.error)
+}
+
+function draw() {
+  // I am just skipping sketch entirely for demonstration purpose,
+  // but you can still continue to show sketch without access to sensors.
+  if (!permissionGranted) return
+
+  // rotationX and rotationY keep track of device orientation (provided by p5js library)
+  cx += map(rotationY, -90, 90, -100, 100)
+  cy += map(rotationX, -90, 90, -100, 100)
+  if (cx >= width) cx = 0
+  else if (cx <= 0) cx = width
+  if (cy >= height) cy = 0
+  else if (cy <= 0) cy = height
+
+  const pulse = sin(frameCount / 20) * 50
+  fill(sin(frameCount/60) * 30 + 30, 100, 100)
+  ellipse(cx, cy, 100 + abs(rotationX*20) + pulse, 100 + abs(rotationX*20) + pulse)
+
 }
